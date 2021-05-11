8965ad6726d89b9693df150e22b967ef2538454a
YASHBRO <yashjoglekar1220@gmail.com>
2021-05-12 00:45:09 +0530

    Basic Website complete

--- English_Thesaurus/urls.py
+++ English_Thesaurus/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = "English Thesaurus Admin"
+admin.site.site_title = "Admin English Thesaurus"
+admin.site.index_title = "Welcome to English Thesaurus Admin Pannel"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Thesaurus_API.urls')),
 ]
--- Thesaurus_API/views.py
+++ Thesaurus_API/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main(request):
+    content = {
+        "word" : "Include",
+        "definitions" : [{
+            "part_of_speech" : "verb" ,
+            "meaning" : "This is meaning of 1st" ,
+            "example" : "This is example of 1st"
+        } ,
+        {
+            "part_of_speech" : "noun" ,
+            "meaning" : "This is meaning of 2nd" ,
+            "example" : "This is example of 2nd"
+        },
+        {
+            "part_of_speech" : "adjective" ,
+            "meaning" : "This is meaning of 3rd" ,
+            "example" : "This is example of 3rd"
+        },
+        {
+            "part_of_speech" : "pronoun" ,
+            "meaning" : "This is meaning of 4th" ,
+            "example" : "This is example of 4th"
+        }]
+    }
+    if request.GET.get('SearchBtn'):
+        print("OK")
+    return render(request,'home.html', content)
